6faeb94a3699cfde563f070d28b6ba5850b2c4b0
tudy <jhh0207@hanmail.net>
2021-03-22 23:29:55 +0900

    chap04. 스택 Push / pop 추가

---
+++ src/chap04/IntStackTester.java
@@ -0,0 +1,15 @@
+package chap04;
+
+import java.util.Scanner;
+
+public class IntStackTester {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        IntStack s = new IntStack(64); // 최소 64개를 푸시할 수 있는 스택
+
+        while(true){
+            System.out.println("현재 데이터 수 : ");
+        }
+
+    }
+}
